9850c2900d332e9e4c0106e0d670cb18538696c6
Alexei.Smirnov <alexei.smirnov@hys-enterprise.com>
2023-05-03 19:20:57 +0300

    Add synch Common vocab and User vocab

--- TelegaEngBot/Services/CheckDb.cs
+++ TelegaEngBot/Services/CheckDb.cs
@@ -2,7 +2,10 @@
 using System.Security.Cryptography;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
+using NLog;
 using TelegaEngBot.DataAccessLayer;
+using TelegaEngBot.Models;
+
 #pragma warning disable CS8604
 
 namespace TelegaEngBot.Services;
@@ -10,6 +13,7 @@ namespace TelegaEngBot.Services;
 public class CheckDb
 {
     private AppDbContext _dbContext;
+    private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
 
     public CheckDb(AppDbContext dbContext)
     {
@@ -33,25 +37,34 @@ public class CheckDb
             .ToArray();
         var commonVocabularyHash = GetHash(commonVocabularyGuids);
         Console.WriteLine("CommonVoc " + PrintHash(commonVocabularyHash));
-        
+
         // Get user vocabulary hash
         foreach (var appUser in _dbContext.UserList)
         {
             if (appUser.TelegramUserId != 450056320) // todo delete this condition
                 continue;
-            
+
+            // if (!appUser.isVocabSync) // todo add new property to AppUser and add check it in code
+            // {
+            //     continue;
+            // }
+
             var userVocabularyGuids = appUser.UserVocabulary
                 .Select(x => x.Article)
-                .Select(x=>x.Id)
+                .Select(x => x.Id)
                 .ToArray();
-  
+
             var userVocabularyHash = GetHash(userVocabularyGuids);
             Console.Write(appUser.TelegramUserId + " " + PrintHash(userVocabularyHash));
-                
-            if (commonVocabularyHash != userVocabularyHash)
+            
+            if (!commonVocabularyHash.SequenceEqual(userVocabularyHash))
             {
                 Console.WriteLine(" - not match");
-                // ReconcileData();
+                SyncVocabularies(appUser);
+                Logger.Info("Common vocabulary and user vocabulary was synchronised for Telegram User Id: " +
+                            appUser.TelegramUserId);
+                Console.WriteLine("Common vocabulary and user vocabulary was synchronised for Telegram User Id: " +
+                                  appUser.TelegramUserId);
             }
             else
             {
@@ -60,9 +73,15 @@ public class CheckDb
         }
     }
 
-    private static void ReconcileData()
+    private void SyncVocabularies(AppUser appUser)
     {
-        
+        foreach (var article in _dbContext.CommonVocabulary)
+        {
+            if (appUser.UserVocabulary.Any(x => x.Article == article))
+                continue;
+            appUser.UserVocabulary.Add(new UserVocabularyItem {Article = article, Weight = 10});
+            _dbContext.SaveChanges();
+        }
     }
 
     private static byte[] GetHash(Guid[] guids)
@@ -83,7 +102,7 @@ public class CheckDb
         {
             sb.Append(t.ToString("x2"));
         }
+
         return sb;
     }
-    
 }
