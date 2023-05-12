96d2359dec3a80031472a48da1223328a6c50187
Alexei.Smirnov <alexei.smirnov@hys-enterprise.com>
2023-05-12 12:00:10 +0300

    Refactor ErrorHandler

--- TelegaEngBot/Handlers/ErrorHandler.cs
+++ TelegaEngBot/Handlers/ErrorHandler.cs
@@ -7,20 +7,24 @@ namespace TelegaEngBot.Handlers;
 internal static class ErrorHandler
 {
     static Logger logger = LogManager.GetCurrentClassLogger();
+
     internal static Task HandleError(ITelegramBotClient client, Exception exception, CancellationToken ct)
     {
         var errorMessage = exception switch
         {
-            ApiRequestException apiRequestException =>
+            Telegram.Bot.Exceptions.RequestException {InnerException: System.Net.Http.HttpRequestException httpException} =>
+                $"Telegram HTTP request error: {httpException.Message}",
+            Telegram.Bot.Exceptions.ApiRequestException apiRequestException =>
                 $"Telegram API error: {apiRequestException.ErrorCode} - {apiRequestException.Message}",
-            HttpRequestException httpException =>
-                $"HTTP request error: {httpException.Message}",
             System.Net.Sockets.SocketException socketException =>
                 $"Network error: {socketException.Message}",
+            System.Net.Http.HttpRequestException httpException =>
+                $"HTTP request error: {httpException.Message}",
             _ => exception.ToString()
         };
-        logger.Fatal(errorMessage);
-        Console.WriteLine(errorMessage);
+
+        logger.Warn(errorMessage);
+        Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.ms") + " " + errorMessage);
         return Task.CompletedTask;
     }
 }
