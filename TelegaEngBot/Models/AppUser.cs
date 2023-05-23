9ec36a0d2072eb138c548725ab9cabcba337fa13
Alexei.Smirnov <alexei.smirnov@hys-enterprise.com>
2023-05-23 18:42:56 +0300

    Add new prop to AppUser

--- TelegaEngBot/Models/AppUser.cs
+++ TelegaEngBot/Models/AppUser.cs
@@ -9,5 +9,7 @@ public class AppUser
     public string TelegramLastName { get; set; }
     public virtual Article LastArticle { get; set; }
     public virtual List<UserVocabularyItem> UserVocabulary { get; set; }
-    public virtual UserSettings UserSettings { get; set; } = null!;
+    public virtual UserSettings UserSettings { get; set; }
+    public bool IsSynced { get; set; }
+    public DateTime LastActivity { get; set; }
 }
--- TelegaEngBot/Models/UserVocabularyItem.cs
+++ TelegaEngBot/Models/UserVocabularyItem.cs
@@ -3,6 +3,6 @@
 public class UserVocabularyItem
 {
     public Guid Id { get; set; }
-    public virtual Article Article { get; set; } = null!;
+    public virtual Article Article { get; set; }
     public int Weight { get; set; }
 }
