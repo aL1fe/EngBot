e418c324c057e6d19ee29275acd2583bb6877a66
Alexei.Smirnov <alexei.smirnov@hys-enterprise.com>
2023-07-06 16:38:45 +0300

    Add env variables

--- TelegaEngBot/AppConfigurations/AppConfig.cs
+++ TelegaEngBot/AppConfigurations/AppConfig.cs
@@ -11,16 +11,29 @@ public static class AppConfig
         var builder = new ConfigurationBuilder()
             .SetBasePath(Directory.GetCurrentDirectory())
             .AddJsonFile("appsettings.json")
+            .AddEnvironmentVariables()
             .AddUserSecrets<Program>();
         _configuration = builder.Build();
     }
+
+    public static string Env => _configuration["ENVIRONMENT"];
+
+    public static string BotToken =>
+        Env == "Production"
+            ? _configuration.GetSection("BotToken")["ProdBot"]
+            : _configuration.GetSection("BotToken")["TestBot"];
+
+    public static string OpenAiToken => _configuration.GetSection("OpenAI")["OpenAIToken"];
+
+    public static string ConnectionString =>
+        Env == "Production"
+            ? _configuration["CONNECTION_STRING"]
+            : _configuration.GetConnectionString("DockerConnection");
+
+    public static string NeuralModelHost =>
+        Env == "Production"
+            ? _configuration["NEURAL_MODEL_HOST"]
+            : _configuration["NeuralModelHost"];
     
-    //From user secrets
-    public static string BotToken => _configuration.GetSection("BotToken")["TestBot"];
-    public static string OpenAIToken => _configuration.GetSection("OpenAI")["OpenAIToken"];
-    
-    //From appsettings.json
-    public static string ConnectionString => _configuration.GetConnectionString("DockerConnection");
-    public static string OpenAIPromt => _configuration.GetSection("OpenAI")["OpenAIPromt"];
-    public static string NeuralModelHost => _configuration["NeuralModelHost"];
+    public static string OpenAiPromt => _configuration.GetSection("OpenAI")["OpenAIPromt"];
 }
